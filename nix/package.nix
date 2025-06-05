ae1b1c9df03f89301d05bc2f2f9f722a1c7d6228
ivan770 <ivan@ivan770.me>
2025-06-05 23:56:19 +0000

    Add meta section to the Nix derivation

--- nix/package.nix
+++ nix/package.nix
@@ -1,4 +1,8 @@
-{ rustPlatform, src }:
+{
+  lib,
+  rustPlatform,
+  src,
+}:
 rustPlatform.buildRustPackage {
   pname = "slicer";
   version = "0.1.0";
@@ -6,4 +10,12 @@ rustPlatform.buildRustPackage {
   inherit src;
 
   cargoHash = "sha256-rMYQDtqz6zpeGNlfPWNA8rNb5jSXmB0vTOqT2JlwlT0=";
+
+  meta = {
+    description = "Run desktop applications as systemd services";
+    homepage = "https://github.com/ivan770/slicer";
+    platforms = lib.platforms.linux;
+    license = lib.licenses.mit;
+    mainProgram = "slicer";
+  };
 }
